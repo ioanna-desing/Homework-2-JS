051b4e298efbdafde8005b0757d5b22dbf9b7248
Ioanna-desing <ioanna@email.ua>
2021-01-05 17:38:53 +0200

    try

---
+++ script2.js
@@ -0,0 +1,23 @@
+// let arrayStr = ['Meri', 'Andriy', 'Daniy','Orest','Azazel'];
+// let arrayNumb = [15, 10, 1, 25, 5];
+// let arrayMix = [40, 'lilit', false, true, - 26, 95, 'Adam', false, 14, 'me', 'photo', 45, true, 'homework', true];
+// let arrayVacuum = [];
+// console.log (arrayStr);
+// console.log(arrayNumb);
+// console.log(arrayMix);
+// arrayVacuum[1] = 'hello';
+// arrayVacuum[5] = 'world';
+// arrayVacuum[3] = 40;
+// arrayVacuum[24] = true;
+// console.log(arrayVacuum);
+
+
+// let n1 = prompt('введите не целое число');
+// let n2 = prompt('введите не целое число');
+// let n3 = prompt('введите не целое число');
+// let result = Math.round(n1+n2+n3);
+// console.log(result);
+
+// let n1 = promt();
+// let n2 = prompt();
+// let n3 = prompt();
